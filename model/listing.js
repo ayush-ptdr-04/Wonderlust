d0b53766b703e7eaa5638584dda8af81d12af7a5
aayush patidar <ap6887980@gmail.com>
2025-10-15 18:16:32 +0530

    update data.js and access URL for image

--- model/listing.js
+++ model/listing.js
@@ -10,13 +10,8 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1683009680116-b5c04463551d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1683009680116-b5c04463551d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
-        : v,
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
